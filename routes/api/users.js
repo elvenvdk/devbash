e22e7a9f854c0a8fcd5c2acdc8cfe10713930f4a
Alvin van der Kuech <alvin.vandekuech@bjss.com>
2019-04-16 17:00:05 -0400

    Added auth dependencies

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,21 +4,23 @@ const pool = require('../../db');
 const router = Router();
 
 // Get all users
+
 router.get('/', (request, response, next) => {
-  pool.query(`SELECT * FROM users`, (err, res) => {
-    if (err) return next(err);
-    response.json(res.rows);
-  });
+  pool
+    .query('SELECT * FROM users ORDER BY id ASC')
+    .then(res => response.json(res.rows))
+    .catch(err => next(err));
 });
 
 // Get user based on id
+
 router.get('/:id', (request, response, next) => {
   console.log(request.params.id);
   const { id } = request.params;
-  pool.query(`SELECT * FROM users WHERE users.id = $1`, [id], (err, res) => {
-    if (err) return next(err);
-    response.json(res.rows);
-  });
+  pool
+    .query(`SELECT * FROM users WHERE users.id = $1`, [id])
+    .then(res => response.json(res.rows))
+    .catch(err => next(err));
 });
 
 // Create user
@@ -32,8 +34,9 @@ router.post('/signup', (request, response, next) => {
     user_password,
     user_password2
   } = request.body;
-  pool.query(
-    `INSERT INTO users(
+  pool
+    .query(
+      `INSERT INTO users(
       first_name, 
       last_name, 
       email, 
@@ -41,12 +44,10 @@ router.post('/signup', (request, response, next) => {
       user_password, 
       user_password2
       ) VALUES($1, $2, $3, $4, $5, $6)`,
-    [first_name, last_name, email, username, user_password, user_password2],
-    (err, res) => {
-      if (err) return next(err);
-      response.status(201).send({ msg: 'Successfully created' });
-    }
-  );
+      [first_name, last_name, email, username, user_password, user_password2]
+    )
+    .then(() => response.redirect('/users'))
+    .catch(err => next(err));
 });
 
 module.exports = router;
